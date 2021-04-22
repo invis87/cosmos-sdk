7c81b20e423fa46e5af8f7b346be042d72c1a593
Stanislav Pirx <stanislav.prix@gmail.com>
2021-04-22 14:52:50 +0300

    add methods to avoid gas calculation
    Signed-off-by: Stanislav Pirx <stanislav.prix@gmail.com>

--- types/context.go
+++ types/context.go
@@ -35,6 +35,9 @@ type Context struct {
 	minGasPrice   DecCoins
 	consParams    *abci.ConsensusParams
 	eventManager  *EventManager
+
+	real_gasMeter  GasMeter
+	real_blockGasMeter GasMeter
 }
 
 // Proposed rename, not done to avoid API breakage
@@ -55,6 +58,20 @@ func (c Context) IsCheckTx() bool             { return c.checkTx }
 func (c Context) IsReCheckTx() bool           { return c.recheckTx }
 func (c Context) MinGasPrices() DecCoins      { return c.minGasPrice }
 func (c Context) EventManager() *EventManager { return c.eventManager }
+
+func (c Context) HackGas()  {
+	c.real_gasMeter = c.gasMeter
+	c.real_blockGasMeter = c.blockGasMeter
+	c.gasMeter = stypes.NewInfiniteGasMeter()
+	c.blockGasMeter = stypes.NewInfiniteGasMeter()
+}
+
+func (c Context) UnHackGas()  {
+	c.gasMeter = c.real_gasMeter
+	c.blockGasMeter = c.real_blockGasMeter
+	c.real_gasMeter = nil
+	c.real_blockGasMeter = nil
+}
 
 // clone the header before returning
 func (c Context) BlockHeader() abci.Header {
